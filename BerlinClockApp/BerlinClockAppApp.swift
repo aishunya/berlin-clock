8d5b256d08c33f19acd3bc72fc7df32ca51b54e8
aishunya <ayshanurgali@gmail.com>
2024-01-30 19:13:11 +0600

    Initial Commit

---
+++ BerlinClockApp/BerlinClockAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BerlinClockAppApp.swift
+//  BerlinClockApp
+//
+//  Created by Aisha Nurgaliyeva on 30.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct BerlinClockAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BerlinClockApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BerlinClockApp
+//
+//  Created by Aisha Nurgaliyeva on 30.01.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
